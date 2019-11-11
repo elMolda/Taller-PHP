91512eb5734ac703c3a9f2f67951eb19cea86417
elMolda <david97molano@gmail.com>
2019-11-10 19:57:03 -0500

    Create Done. Update fields Done

--- manage_people.php
+++ manage_people.php
@@ -12,16 +12,40 @@ if($conn){
     $result = $conn->query($sql);
     $flag = FALSE;
     while($row = mysqli_fetch_array($result) and !$flag) {
-        if($cedula == $row){
+        if($cedula == $row["cedula"]){
             $flag = TRUE; //Registered
         }
     }
-
-    if($flag){//Update non blank fields
     
+    if($flag){//Update non blank fields
+        if($nombre != ""){
+            $stmt = $conn->prepare("UPDATE Personas SET nombre = ? WHERE cedula = ?");
+            $stmt->bind_param("si", $nombre, $cedula);
+            $stmt->execute();
+            echo "Se ha actualizado el nombre correctamente a " . $nombre;
+        }
+        if($apellido != ""){
+            $stmt = $conn->prepare("UPDATE Personas SET apellido = ? WHERE cedula = ?");
+            $stmt->bind_param("si", $apellido, $cedula);
+            $stmt->execute();
+            echo "Se ha actualizado el apellido correctamente a " . $apellido;
+        }
+        if($correo != ""){
+            $stmt = $conn->prepare("UPDATE Personas SET correo = ? WHERE cedula = ?");
+            $stmt->bind_param("si", $correo, $cedula);
+            $stmt->execute();
+            echo "Se ha actualizado el correo correctamente a " . $correo;
+        }
+        if($edad != ""){
+            $stmt = $conn->prepare("UPDATE Personas SET edad = ? WHERE cedula = ?");
+            $stmt->bind_param("ii", $edad, $cedula);
+            $stmt->execute();
+            echo "Se ha actualizado la edad correctamente a " . $edad;
+        }
+        
     }else{//Create 
         $stmt = $conn->prepare("INSERT INTO `Personas` (`cedula`, `nombre`, `apellido`, `correo`, `edad`) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $cedula, $nombre, $apellido, $correo, $edad);
+        $stmt->bind_param("isssi", $cedula, $nombre, $apellido, $correo, $edad);
         $stmt->execute();
         echo "Se ha insertado correctamente a " . $nombre . " " . $apellido;
     }
@@ -30,5 +54,7 @@ if($conn){
 }else{
 
 }
+
+
 
 ?>

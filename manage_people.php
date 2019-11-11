6923c5212908ec548de3bc966cd6e63354986f33
elMolda <david97molano@gmail.com>
2019-11-10 19:07:55 -0500

    Create pearson works

--- manage_people.php
+++ manage_people.php
@@ -2,17 +2,33 @@
 include_once dirname(__FILE__) . '/config.php'; //Import credentials
 $conn = mysqli_connect(HOST_DB,USER_DB,USER_PASS,NAME_DB); //Connect to DB
 
-if ($_GET) {
 if($conn){
+    $cedula = $_POST["cedula"];
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $correo = $_POST["correo"];
+    $edad = $_POST["edad"];
     $sql = "SELECT cedula FROM Personas";
     $result = $conn->query($sql);
-    while($row = mysqli_fetch_array($result)) {
-        echo $row ['cedula'];
-        echo "<br>";
+    $flag = FALSE;
+    while($row = mysqli_fetch_array($result) and !$flag) {
+        if($cedula == $row){
+            $flag = TRUE; //Registered
+        }
     }
+
+    if($flag){//Update non blank fields
+    
+    }else{//Create 
+        $stmt = $conn->prepare("INSERT INTO `Personas` (`cedula`, `nombre`, `apellido`, `correo`, `edad`) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $cedula, $nombre, $apellido, $correo, $edad);
+        $stmt->execute();
+        echo "Se ha insertado correctamente a " . $nombre . " " . $apellido;
+    }
+
 
 }else{
 
 }
-}
+
 ?>

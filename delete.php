b96b4696fcdfb19d42ed626ca08c746aa65a1858
elMolda <david97molano@gmail.com>
2019-11-10 20:31:22 -0500

    People CRUD done

---
+++ delete.php
@@ -0,0 +1,14 @@
+<?php
+include_once dirname(__FILE__) . '/config.php'; //Import credentials
+$conn = mysqli_connect(HOST_DB,USER_DB,USER_PASS,NAME_DB); //Connect to DB
+
+if($conn){
+    $cedula = $_POST["cedula"];
+    $stmt = $conn->prepare("DELETE FROM Personas WHERE cedula = ?");
+    $stmt->bind_param("i", $cedula);
+    $stmt->execute();
+    echo "Se ha eliminado correctamente a " . $cedula;
+}else{
+
+}
+?>
